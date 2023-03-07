9fb0358e18d4f9f087ff90c8800380f02e07903b
mariami <mariam.kavtaradze978@gmail.com>
2023-03-07 22:52:38 +0400

    add file

---
+++ script.js
@@ -0,0 +1,49 @@
+// let first = prompt ('please enter: ') 
+// let second = prompt ('please enter: ')
+
+// function tolia (first, second) {
+//     if (typeof(first)===typeof(second)){
+//         if (first===second){
+//             console.log('first tolia second');
+//         } else if (first !== second){
+//             console.log('first ar udris second');
+//         }
+//     }
+// }
+// console.log(tolia(first, second))
+
+
+
+
+// let farenheit = prompt('Enter temperature in Farenheit: ');
+
+// function temperature (farenheit) {
+//     let C =(farenheit-32)*5/9;
+//     let rounded = C.toFixed(1);
+//     console.log(rounded);
+// }
+// console.log(temperature(farenheit))
+
+
+
+// let first = prompt ('first number: ')
+// let operator = prompt ('Enter Operator: ')
+// let second = prompt ('second number: ')
+
+// function calculator (first, second, operator) {
+//     if (operator=='+'){
+//         return first + second;
+//     } else if (operator=='-'){
+//         return first - second;
+//     } else if (operator=='*'){
+//         return first * second;
+//     } else if (operator=='/'){
+//         return first / second;
+//     }
+// }
+// console.log(calculator(first, second, operator))
+
+
+
+
+
